5616d7f30792b3d4c4a4105505ff9a702d77c9cd
Paloma <paloma166.pah@gmail.com>
2025-02-11 17:21:30 -0300

    Adicionando produtos ao carrinho

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -1,4 +1,5 @@
 /// <reference types="cypress" />
+import produtosPage from "../support/page_objects/produtos.page";
 
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
   /*  Como cliente 
@@ -10,13 +11,40 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
       E validando minha compra ao final */
 
   beforeEach(() => {
-      cy.visit('/')
+      cy.visit('produtos')
   });
 
-  it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-      //TODO: Coloque todo o fluxo de teste aqui, considerando as boas práticas e otimizações
-      
+  it('Buscar o 1° produto na lista, preenchendo as opções e adicionando ao carrinho', () => {
+    produtosPage.buscarProdutos('Autumn Pullie')
+    cy.get('.search > .tbay-search-form > .form-ajax-search > .form-group > .input-group > .button-group > .button-search').click()
+    produtosPage.addProdutoCarrinho('M', 'Green', 1)
+    });
+
+  it('Buscar o 2° produto na lista, preenchendo as opções e adicionando ao carrinho', () => {
+    produtosPage.buscarProdutoLista('Aether Gym Pant')
+    cy.get('.woocommerce-tabs').should('contain', 'The Aether Gym Pant is built for the studio,')
+    produtosPage.addProdutoCarrinho('36', 'Brown', 1)
+    cy.get('.woocommerce-message').should('exist')
   });
+
+  it('Buscar o 3° produto na lista, preenchendo as opções e adicionando ao carrinho', () => {
+    produtosPage.visitarProduto('Zoltan Gym Tee')
+    produtosPage.addProdutoCarrinho('XS', 'Blue', 1)
+    cy.get('.woocommerce-message').should('exist')
+  });
+
+  it('Buscar o 4° produto na lista, preenchendo as opções e adicionando ao carrinho', () => {
+    cy.fixture('produtos').then(dados =>{
+        produtosPage.buscarProdutos(dados[0].nomeProduto)
+        cy.get('.search > .tbay-search-form > .form-ajax-search > .form-group > .input-group > .button-group > .button-search').click()
+        produtosPage.addProdutoCarrinho(
+            dados[0].tamanho,
+            dados[0].cor,
+            dados[0].quantidade)
+        cy.get('.woocommerce-message').should('contain', dados[0].nomeProduto)
+    })
+  });
+
 
 
 })
