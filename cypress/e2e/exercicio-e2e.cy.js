14d1c0ecf05f8955595285698456cc5a5b2d1252
Paloma <paloma166.pah@gmail.com>
2025-02-13 14:17:58 -0300

    E2E com apenas um cenário it

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -14,37 +14,31 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
       cy.visit('produtos')
   });
 
-  it('Buscar o 1° produto na lista, preenchendo as opções e adicionando ao carrinho', () => {
+  it('Buscar os produto, preencher as opções e adicionar ao carrinho', () => {
     produtosPage.buscarProdutos('Autumn Pullie')
     cy.get('.search > .tbay-search-form > .form-ajax-search > .form-group > .input-group > .button-group > .button-search').click()
     produtosPage.addProdutoCarrinho('M', 'Green', 1)
-    });
+    cy.get('.woocommerce-message').should('exist')
 
-  it('Buscar o 2° produto na lista, preenchendo as opções e adicionando ao carrinho', () => {
+    produtosPage.visitarUrl()
     produtosPage.buscarProdutoLista('Aether Gym Pant')
     cy.get('.woocommerce-tabs').should('contain', 'The Aether Gym Pant is built for the studio,')
     produtosPage.addProdutoCarrinho('36', 'Brown', 1)
     cy.get('.woocommerce-message').should('exist')
-  });
 
-  it('Buscar o 3° produto na lista, preenchendo as opções e adicionando ao carrinho', () => {
     produtosPage.visitarProduto('Zoltan Gym Tee')
     produtosPage.addProdutoCarrinho('XS', 'Blue', 1)
     cy.get('.woocommerce-message').should('exist')
-  });
 
-  it('Buscar o 4° produto na lista, preenchendo as opções e adicionando ao carrinho', () => {
     cy.fixture('produtos').then(dados =>{
-        produtosPage.buscarProdutos(dados[0].nomeProduto)
-        cy.get('.search > .tbay-search-form > .form-ajax-search > .form-group > .input-group > .button-group > .button-search').click()
-        produtosPage.addProdutoCarrinho(
-            dados[0].tamanho,
-            dados[0].cor,
-            dados[0].quantidade)
-        cy.get('.woocommerce-message').should('contain', dados[0].nomeProduto)
-    })
+      produtosPage.buscarProdutos(dados[0].nomeProduto)
+      cy.get('.search > .tbay-search-form > .form-ajax-search > .form-group > .input-group > .button-group > .button-search').click()
+      produtosPage.addProdutoCarrinho(
+          dados[0].tamanho,
+          dados[0].cor,
+          dados[0].quantidade)
+      cy.get('.woocommerce-message').should('contain', dados[0].nomeProduto)
+
+    });
   });
-
-
-
 })
